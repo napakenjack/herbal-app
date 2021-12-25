1f0d9d0e1ff5cfdd0b7b354c33f95ff068c85ca5
napakenjack <63636145+napakenjack@users.noreply.github.com>
2021-12-25 13:10:30 +0900

    updates

--- src/components/ThirdArticle.js
+++ src/components/ThirdArticle.js
@@ -1,11 +1,42 @@
-import './ThirdArticle.css'
+import classes from './ThirdArticle.module.scss'
 
 const ThirdArticle = () => {
     return (
         <>
-            <div className='third_art_cont'>
-                <div>
-                    Suka
+            <div className={classes.third_art_cont}>
+                <div className={classes.article_field}>
+                    <div className={classes.adv_image}>
+                        Place for image
+                    </div>
+                    <div className={classes.adv_title}>
+                        Title Text
+                    </div>
+                    <div className={classes.adv_article}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                    qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>
+                <div className={classes.article_field}>
+                <div className={classes.adv_image}>
+                        Place for image
+                    </div>
+                    <div className={classes.adv_title}>
+                        Title Text
+                    </div>
+                    <div className={classes.adv_article}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                    qui officia deserunt mollit anim id est laborum.
+                    </div>
                 </div>
             </div>
         </>
