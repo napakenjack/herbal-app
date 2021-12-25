1e1aaa0f6bd7b862ed14ced6b714844300c209ab
napakenjack <63636145+napakenjack@users.noreply.github.com>
2021-12-25 19:43:35 +0900

    updates

--- src/components/ThirdArticle.js
+++ src/components/ThirdArticle.js
@@ -1,44 +1,28 @@
+import {advItem} from "../data"
+
 import classes from './ThirdArticle.module.scss'
 
 const ThirdArticle = () => {
     return (
-        <>
-            <div className={classes.third_art_cont}>
-                <div className={classes.article_field}>
-                    <div className={classes.adv_image}>
-                        Place for image
-                    </div>
+        <>{advItem.map((card,index) =>{
+            return(
+                <div key={index} className={classes.third_art_cont}>
+                <div className={classes.article_field}>                    
+                    <img src={card.img} className={classes.card_image}/>
+                    <div className={classes.display_cont}>
                     <div className={classes.adv_title}>
-                        Title Text
+                        {card.title}
                     </div>
                     <div className={classes.adv_article}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                    qui officia deserunt mollit anim id est laborum.
+                    {card.desc}
                     </div>
-                </div>
-                <div className={classes.article_field}>
-                <div className={classes.adv_image}>
-                        Place for image
+                    <button className={classes.det_btn}>詳細</button>
                     </div>
-                    <div className={classes.adv_title}>
-                        Title Text
-                    </div>
-                    <div className={classes.adv_article}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                    qui officia deserunt mollit anim id est laborum.
-                    </div>
+                    
                 </div>
             </div>
+            )
+        })}
         </>
     )
 }
--- src/data.js
+++ src/data.js
@@ -87,3 +87,20 @@ export const popularProducts = [
         bg: "fbf0f4",
     }
 ]
+
+export const advItem = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80",
+        title: "Title Text",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        bg: "fbf0f4",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1504204267155-aaad8e81290d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        title: "Title Text",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        bg: "fbf0f4",
+    },
+]
